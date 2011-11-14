4ece86ccf54258620cf0e1526b61bae9f16b5cd8
Dave <admin@bukkitdev.com>
2011-11-14 19:37:15 +0000

    Added config.yml creation

--- src/me/herghost/Fiery/Fiery.java
+++ src/me/herghost/Fiery/Fiery.java
@@ -1,5 +1,6 @@
 package me.herghost.Fiery;
 
+import java.io.File;
 import java.util.logging.Logger;
 
 import me.herghost.Fiery.commands.giveCommand;
@@ -7,19 +8,46 @@ import me.herghost.Fiery.commands.itemCommand;
 import me.herghost.Fiery.commands.kickCommand;
 import me.herghost.Fiery.commands.spawnCommand;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Fiery extends JavaPlugin {
 	
 	Logger log = Logger.getLogger("Minecraft");
 	
+	 
+	
 	public void onEnable(){ 
 		log.info("Fiery Plugin Enabled");
+		
+		FileConfiguration config;
+		try
+			{
+			config = getConfig();
+			File Fiery = new File("plugins" + File.separator + "Fiery" + File.separator + "config.yml");
+			Fiery.mkdir();
+			saveConfig();
+			}
+		
+		catch
+			(Exception e1)
+				{
+					e1.printStackTrace();
+				}
+		
+		
+		
 		
 		this.getCommand("item").setExecutor(new itemCommand());
 		this.getCommand("give").setExecutor(new giveCommand());
 		this.getCommand("kick").setExecutor(new kickCommand());
 		this.getCommand("spawn").setExecutor(new spawnCommand());
+		
+		
+		
+		
+		
 		
 	}
 	
