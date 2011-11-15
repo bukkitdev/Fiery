d0d1b990532b72e9e5c3f2f221240113640c2e68
Dave <admin@bukkitdev.com>
2011-11-15 23:13:57 +0000

    Connect to database and add username/ip

---
+++ src/me/herghost/Fiery/FieryPlayerListener.java
@@ -0,0 +1,57 @@
+package me.herghost.Fiery;
+
+import java.net.InetSocketAddress;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerListener;
+
+public class FieryPlayerListener extends PlayerListener 
+
+{
+	
+	String user = "bukkitdev";
+	String pass = "bukkitdev";
+	String url = "jdbc:mysql://localhost:3306/Fiery"; 
+	
+	public Fiery plugin;
+	
+	public FieryPlayerListener(Fiery instance)
+	{
+		plugin = instance;
+	}
+	
+	
+	
+	public void onPlayerJoin(PlayerJoinEvent event)
+	{
+		String thisplayer = event.getPlayer().getName();
+		InetSocketAddress playerip = event.getPlayer().getAddress();
+		
+		try
+		{
+			Connection conn = DriverManager.getConnection(url, user, pass); 
+			ResultSet rs =conn.createStatement().executeQuery("SELECT * FROM users WHERE p_name = '" + thisplayer + "'");
+			if (!rs.next())
+			{
+				
+				PreparedStatement sampleQueryStatement = conn.prepareStatement("INSERT INTO users (p_name,p_ip) VALUES ('" + thisplayer + "','" + playerip + "')");
+		        sampleQueryStatement.executeUpdate(); 
+				sampleQueryStatement.close(); 
+				conn.close(); 
+				
+			}
+	    } 
+	
+	catch
+		(SQLException e1) 
+			{
+				e1.printStackTrace();
+        	}
+	}
+}
