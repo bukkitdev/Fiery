fb9d994b139bbbe06c1ad900c5359e9548bd6ac5
Dave <admin@bukkitdev.com>
2011-11-15 23:13:41 +0000

    Added PlayerListener

--- src/me/herghost/Fiery/Fiery.java
+++ src/me/herghost/Fiery/Fiery.java
@@ -16,20 +16,15 @@ import me.herghost.Fiery.functions.sqlFunctions;
 
 
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.Event;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Fiery extends JavaPlugin {
 	
 	Logger log = Logger.getLogger("Minecraft");
-	
-	
-	
-	
-	
-	
-	
-	
-	 
+
+	private final FieryPlayerListener playerListener = new FieryPlayerListener(this);	
 	
 	public void onEnable(){ 
 		log.info("Fiery Plugin Enabled");
@@ -45,26 +40,27 @@ public class Fiery extends JavaPlugin {
 			}
 		
 		catch
-			(Exception e1)
+			(Exception e)
 				{
-					e1.printStackTrace();
+			e.printStackTrace();
 				}
 		
+		//Create Tables
 		
 		
 		try 
 			{
 			    sqlFunctions method = new sqlFunctions();
-	        	method.create_tables(); 
+	        	method.create_tables();
+	        	log.info("Fiery Plugin Database OK!");
 			} 
 		
 		catch
-			(SQLException e) 
+			(SQLException e1) 
 				{
-	        		e.printStackTrace();
+					e1.printStackTrace();
 	        	}
 		
-				
 		this.getCommand("item").setExecutor(new itemCommand());
 		this.getCommand("give").setExecutor(new giveCommand());
 		this.getCommand("kick").setExecutor(new kickCommand());
@@ -72,6 +68,9 @@ public class Fiery extends JavaPlugin {
 		this.getCommand("ban").setExecutor(new banCommand());
 		this.getCommand("unban").setExecutor(new unbanCommand());
 		
+		
+		PluginManager pm = this.getServer().getPluginManager();
+		pm.registerEvent(Event.Type.PLAYER_JOIN,playerListener, Event.Priority.Normal, this);
 		
 		
 		
