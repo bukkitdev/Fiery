c2668b7b1810917d0600db64822ce21124b90d51
Dave <admin@bukkitdev.com>
2011-11-17 09:45:46 +0000

    SetHome Command - Finished

---
+++ src/me/herghost/Fiery/commands/sethomeCommand.java
@@ -0,0 +1,100 @@
+package me.herghost.Fiery.commands;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class sethomeCommand implements CommandExecutor {
+	
+	Logger log = Logger.getLogger("Minecraft");
+	
+
+	String user = "bukkitdev";
+	String pass = "bukkitdev";
+	String url = "jdbc:mysql://localhost:3306/Fiery"; 
+
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+		readCommand((Player) sender, commandLabel, args);
+		return true;
+
+	}
+	
+	private void readCommand(Player player, String cmd, String[] args )
+	{
+		
+		if(cmd.equalsIgnoreCase("sethome")&& player instanceof Player)
+		{
+			if(args.length == 0)
+			{
+			Player p = (Player) player;
+			double x = p.getLocation().getX();
+			double y = p.getLocation().getY();
+			double z = p.getLocation().getZ();
+			try
+			{
+				Connection conn = DriverManager.getConnection(url, user, pass); 
+				PreparedStatement sampleQueryStatement = conn.prepareStatement("REPLACE INTO userhomes SET p_name = '" + p.getName() + "', home_x = '" + x + "', home_y = '" + y + "', home_z = '" + z + "'"); 
+				sampleQueryStatement.executeUpdate(); 
+				sampleQueryStatement.close();
+				p.sendMessage("Home Set Successfully");
+				
+						
+					}
+				
+			
+			
+			catch
+			(SQLException e1) 
+			{
+				e1.printStackTrace();
+        	}
+			
+			
+		}
+			if(args.length == 1)
+			{
+				Player p = (Player) player;
+				double x = p.getLocation().getX();
+				double y = p.getLocation().getY();
+				double z = p.getLocation().getZ();
+				try
+				{
+					Connection conn = DriverManager.getConnection(url, user, pass); 
+					PreparedStatement sampleQueryStatement = conn.prepareStatement("REPLACE INTO userhomes SET p_name ='" + p.getName() +"', home"+args[0]+"_x="+x+",home"+args[0]+"_y ="+y+",home"+args[0]+"_z ="+z+""); 
+					sampleQueryStatement.executeUpdate(); 
+					sampleQueryStatement.close();
+					p.sendMessage("Home " + args[0] + " Set Successfully");
+					
+							
+						}
+					
+				
+				
+				catch
+				(SQLException e1) 
+				{
+					e1.printStackTrace();
+	        	}
+				
+				
+			}
+				
+		}
+	}}
+		
+	
+	
+
+
+
