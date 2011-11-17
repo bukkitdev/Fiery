041788cd7ace919907e48fdf0f7832eabcbd8e77
Dave <admin@bukkitdev.com>
2011-11-17 09:45:34 +0000

    updated varchar lengths for homes

--- src/me/herghost/Fiery/functions/sqlFunctions.java
+++ src/me/herghost/Fiery/functions/sqlFunctions.java
@@ -16,11 +16,20 @@ public class sqlFunctions {
 	String pass = "bukkitdev";
 	String url = "jdbc:mysql://localhost:3306/Fiery"; 
 	
-	public void create_tables() throws SQLException
+	public void create_table_users() throws SQLException
 	{ 
 		Connection conn = DriverManager.getConnection(url, user, pass); 
 		PreparedStatement sampleQueryStatement = conn.prepareStatement("CREATE TABLE IF NOT EXISTS users (p_id int NOT NULL AUTO_INCREMENT KEY, p_name varchar(255), p_ip varchar(255))"); 
-		sampleQueryStatement.executeUpdate(); 
+		sampleQueryStatement.executeUpdate();
+		sampleQueryStatement.close(); 
+		conn.close(); 
+	}
+	
+	public void create_table_userhomes() throws SQLException
+	{
+		Connection conn = DriverManager.getConnection(url, user, pass); 
+		PreparedStatement sampleQueryStatement = conn.prepareStatement("CREATE TABLE IF NOT EXISTS userhomes (p_name varchar(255), home_x varchar(500), home_y varchar(500), home_z varchar(500), home1_x varchar(500), home1_y varchar(500), home1_z varchar(500), home2_x varchar(500), home2_y varchar(500), home2_z varchar(500))");
+		sampleQueryStatement.executeUpdate();
 		sampleQueryStatement.close(); 
 		conn.close(); 
 	}
