3119a1fb2357cd87fff510de4fa40582d6d67cfe
Dave <admin@bukkitdev.com>
2011-11-28 19:27:31 +0000

    Non admin reload

---
+++ src/me/herghost/Fiery/commands/mreloadCommand.java
@@ -0,0 +1,24 @@
+package me.herghost.Fiery.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class mreloadCommand implements CommandExecutor {
+
+	@Override
+public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+	
+	{
+		if(cmd.getName().equalsIgnoreCase("mreload")&& sender instanceof Player)
+		{
+			 Bukkit.reload();
+			 sender.sendMessage(ChatColor.GREEN + "Reload complete.");
+		}
+		return true;
+	}
+
+}
